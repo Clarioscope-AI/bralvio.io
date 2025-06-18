3c35d98c9e45f0dcf8b855cd69085f3a109d28d6
Lenovo <jannatjui414@gmail.com>
2025-06-18 23:49:01 +0600

    feat: add SecondaryButton component with hover arrow animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import PrimaryButton from "./components/ui/Button/PrimaryButton";
+
 export default function Home() {
   return (
     <div>
       Hello from bralvio
+      {/* call the button component for testing */}
+      <div className="flex justify-center items-center gap-4">
+        <PrimaryButton
+          label="Book a Demo"
+          arrow={true}
+          classNames="hover:bg-purple-100"
+        />
+        <PrimaryButton
+          label="Book a Demo"
+          arrow={true}
+          classNames=" bg-tertiary text-secondary border-2 border-purple-800 hover:bg-purple-700"
+        />
+      </div>
     </div>
   );
 }
