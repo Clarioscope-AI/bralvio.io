2cb330c66cc9222e7a03257599dd0884edd245bd
Lenovo <jannatjui414@gmail.com>
2025-06-19 17:13:53 +0600

    feat: make hero component of home page

---
+++ components/hero/Hero.tsx
@@ -0,0 +1,34 @@
+import React, { JSX } from "react";
+import { heroData } from "@/data/heroData";
+import Button from "../ui/button/Button";
+
+const Hero: React.FC = (): JSX.Element => {
+  const heroTitle = heroData.title;
+  const keyword = "Government AI";
+
+  const [before, after] = heroTitle.split(keyword);
+  return (
+    <section className="flex flex-col items-center justify-center text-center p-8 h-[300x] lg:h-[550px] border-2">
+      <h1 className="lg:w-1/2 mx-auto text-3xl md:text-4xl lg:text-7xl">
+        {before}
+        <span className="text-blue-600">{keyword}</span>
+        {after}
+      </h1>
+      <p className="w-2/3 lg:w-1/2 mx-auto my-6">{heroData.description}</p>
+      <div className="flex gap-2">
+        <Button
+          label="Book a Demo"
+          arrow
+          classNames="border-2 rounded-lg mt-4 p-2 lg:p-3"
+        ></Button>
+        <Button
+          label="Build AI"
+          arrow
+          classNames="border-2 rounded-lg mt-4 p-2 lg:p-3"
+        ></Button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
