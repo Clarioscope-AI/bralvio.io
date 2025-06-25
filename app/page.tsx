5c2da6605e1fbde8c0e636bfa9fa43ce0a3ce557
Lenovo <jannatjui414@gmail.com>
2025-06-25 20:54:04 +0600

    feat:create ai  partner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/hero/Hero";
 import HeroTab from "@/components/heroTab/HeroTab";
 import Header from "@/components/layout/header/header";
+import Partner from "@/components/partner/Partner";
 export default function Home() {
   return (
     <>
       <Header></Header>
       <Hero></Hero>
       <HeroTab></HeroTab>
+      <Partner></Partner>
     </>
   );
 }
---
+++ data/partnerData.ts
@@ -0,0 +1,8 @@
+import img from "@/assets/partner/openai.webp";
+import img2 from "@/assets/partner/adept.webp";
+import img3 from "@/assets/partner/meta.webp";
+import img4 from "@/assets/partner/cohere.webp";
+import img5 from "@/assets/partner/anthropic.webp";
+import { StaticImageData } from "next/image";
+
+export const partners: StaticImageData[] = [img, img2, img3, img4, img5];
