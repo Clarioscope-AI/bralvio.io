0372f8c4a3664730a8898850d96b44d0f7073633
Lenovo <jannatjui414@gmail.com>
2025-06-26 10:57:00 +0600

    fix: remove comented code

--- components/aiResearch/Slider.tsx
+++ components/aiResearch/Slider.tsx
@@ -4,9 +4,7 @@ import "keen-slider/keen-slider.min.css";
 
 import Image from "next/image";
 import { AiOutlineSelect } from "react-icons/ai";
-// import slideimg from "@/assets/setting.webp";
-// import slideimg2 from "@/assets/slideImg.webp";
-// import slideimg3 from "@/assets/slideImg3.webp";
+
 import { slides } from "@/data/sliderData";
 const Slider = () => {
   const [sliderRef] = useKeenSlider({
@@ -17,50 +15,6 @@ const Slider = () => {
       spacing: 15,
     },
   });
-  // interface SlideData {
-  //   title: string;
-  //   subtitle: string;
-  //   image: StaticImageData | string;
-  //   link: string;
-  // }
-  // const slides: SlideData[] = [
-  //   {
-  //     title: "Leaderboards",
-  //     subtitle: "SEAL Leaderboards: Expert-Driven Private Evaluations",
-  //     image: slideimg,
-  //     link: "",
-  //   },
-  //   {
-  //     title: "Research",
-  //     subtitle: "LLMs Easily Jailbroken as Browser Agents",
-  //     image: slideimg2,
-  //     link: "",
-  //   },
-  //   {
-  //     title: "Research",
-  //     subtitle: "Multi-Turn Human Jailbreaks on LLM Defenses",
-  //     image: slideimg3,
-  //     link: "",
-  //   },
-  //   {
-  //     title: "Research",
-  //     subtitle: "Examination of LLM Performance on Grade School Arithmetic",
-  //     image: slideimg,
-  //     link: "",
-  //   },
-  //   {
-  //     title: "Research",
-  //     subtitle: "Examination of LLM Performance on Grade School Arithmetic",
-  //     image: slideimg3,
-  //     link: "",
-  //   },
-  //   {
-  //     title: "Research",
-  //     subtitle: "Examination of LLM Performance on Grade School Arithmetic",
-  //     image: slideimg2,
-  //     link: "",
-  //   },
-  // ];
 
   return (
     <div ref={sliderRef} className="keen-slider my-20">
