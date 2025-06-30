a453246b2ce9b8f2f22ba10489c4231c5002864e
Lenovo <jannatjui414@gmail.com>
2025-06-30 20:15:07 +0600

    feat: add datacuration section

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       <AiResearch></AiResearch>
       <Partner></Partner>
       <DataEngine></DataEngine>
+      <DataCuration></DataCuration>
       <AiApplication></AiApplication>
     </>
   );
